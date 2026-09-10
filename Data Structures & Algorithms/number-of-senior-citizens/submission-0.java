1a0981f834227a34fa0a50c643948bd55c8a6356
Avanish Mani Tripathi <96742231+avanishmani@users.noreply.github.com>
2026-09-10 22:06:02 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count =0;
+        for(String st: details){
+            int age = Integer.parseInt(st.substring(11,13));
+            // System.out.println(st + "  -> "+ age);
+            if(age>60){
+                count++;
+                }
+        }
+        return count;
+    }
+}
